7d40232a95a1188ad5db32c450b203cc44c305a1
GOG <gogckhs@gmail.com>
2024-06-06 16:38:45 +0800

    修改103架構，新增104_eg

--- 第一章 基礎設定與基礎運動學/103_free falling/103_free falling pc2.py
+++ 第一章 基礎設定與基礎運動學/103_free falling/103_free falling pc2.py
@@ -17,14 +17,12 @@ a = 0
 '''
 scene = canvas(center = vec(0,0,0), title = "free falling", width = 1200, height = 800, background = vec(1, 0, 0))
 floor = box(pos = vec(0,0,0), size = vec(l, l, 0.1*radius), color = color.blue)
-ball = sphere(pos = vec(0,0,40.0), radius = radius, color = vec(0.5,0.5,0.5))
-# 這裡的sphere是指「球體」，其中參數「radius」是球的半徑
+ball = sphere(pos = vec(0,0,40), radius = radius, color = vec(0.5,0.5,0.5))
 
 yplot = graph(title = "y-t plot", xtitle = "t(s)", ytitle = "y(m)", x = 0, y = 800, width = 800, height = 600)
 vplot = graph(title = "v-t polt", xtitle = "t(s)", ytitle = "v(m/s)", x = 0, y = 800, width = 800, height = 600)
 g1 = gcurve(graph = yplot, color = color.red)
 g2 = gcurve(graph = vplot, color = color.red)
-#上面的在102_basic motion cm都有提過了
 
 '''
  3. 物體運動部分, 碰撞次數到達20次即停止
@@ -39,5 +37,6 @@ while ball.pos.z >= (0.5*floor.size.z + radius):
     g1.plot(pos=(t, ball.pos.z))
     g2.plot(pos = (t, v))
 
+ball.pos.z = 0.5*floor.size.z + radius
 print ("t = ", t)
 print ("v = ", v)
