82126231008ed99ba543451a374a7a36e6a79653
GOG <gogckhs@gmail.com>
2024-06-06 18:20:57 +0800

    新增105_eg, 105_pc, 修改修改104架構

--- 第一章 基礎設定與基礎運動學/104_horizontal throw/104_ horizontal throw pc.py
+++ 第一章 基礎設定與基礎運動學/104_horizontal throw/104_ horizontal throw pc.py
@@ -1,6 +1,6 @@
 from vpython import *
 
-l = 100
+l = 150
 radius = 1
 g = -0.98
 v = vec(10, 0, 0)
@@ -9,6 +9,8 @@ t = 0
 dt = 0.001
 b = 0.1
 m = 1
+e = 0.8
+collapse = 0
 
 back = canvas(center = vec(0,0,0), background = vec(0.3,0.3,0.7), width =  800, height = 600, title = "ideal horizontal throw")
 floor = box(pos = vec(0,0,0), size = vec(l,0.1*radius,0.1*l), color = vec (0.5, 0.6,0.3))
@@ -18,13 +20,14 @@ x = arrow(axis = vec(1,0,0), length= 5, pos = vec(0,2,20))
 z = arrow(axis = vec(0,1,0), length = 5, pos = vec(0,2,20))
 
 xyplot = graph(title = "x-y plot", xtitle = "x(m)", ytitle = "y(m)", height = 400, width = 600)
-vtplot = graph(title = "v-t plot", xtitle = "t(s)", ytitle = "black:vx(m/s) ; blue:vy(m/s)", height = 400, width = 600)
+vtplot = graph(title = "vx-t plot", xtitle = "t(s)", ytitle = "vx(m/s)", height = 400, width = 600)
+vytplot = graph(title = "vy-t plot", xtitle = "t(s)", ytitle = "vy(m/s)", height = 400, width = 600)
 
 xy = gcurve(graph = xyplot, color = color.red)
 vxt = gcurve(graph = vtplot, color = color.black)
-vyt = gcurve(graph = vtplot, color = color.blue)
+vyt = gcurve(graph = vytplot, color = color.blue)
 
-while ball.pos.y >= (floor.size.y*0.5 + ball.radius):
+while collapse <= 5:
     rate(1000)
     t += dt
     a = vec(0,g,0) - b*v
@@ -33,4 +36,8 @@ while ball.pos.y >= (floor.size.y*0.5 + ball.radius):
 
     xy.plot(ball.pos.x, ball.pos.y)
     vxt.plot(pos = (t,v.x))
-    vyt.plot(pos = (t, abs(v.y)))
+    vyt.plot(pos = (t, v.y))
+    if ball.pos.y <= (floor.size.y*0.5 + ball.radius):
+        v.y = -e*v.y 
+        collapse += 1
+        ball.pos.y = floor.size.y*0.5 + ball.radius
--- 第一章 基礎設定與基礎運動學/105_diagonal throw/105_diagonal throw pc.py
+++ 第一章 基礎設定與基礎運動學/105_diagonal throw/105_diagonal throw pc.py
@@ -3,28 +3,31 @@ from vpython import *
 l = 100
 radius = 1
 g = -0.98
-v = vec(10, 0, 0)
+v = vec(10, 10, 0)
 a = vec(0, g, 0)
 t = 0
 dt = 0.001
 b = 0.1
 m = 1
+e = 0.8
+collapse = 0
 
 back = canvas(center = vec(0,0,0), background = vec(0.3,0.3,0.7), width =  800, height = 600, title = "ideal horizontal throw")
 floor = box(pos = vec(0,0,0), size = vec(l,0.1*radius,0.1*l), color = vec (0.5, 0.6,0.3))
-ball = sphere(radius = radius, pos = vec(-floor.size.x*0.5,49,0), color = color.red, make_trail=True, trail_color = color.white, v = v, a = a)
+ball = sphere(radius = radius, pos = vec(-floor.size.x*0.5,floor.pos.y*0.5+radius,0), color = color.red, make_trail=True, trail_color = color.white, v = v, a = a)
 
 x = arrow(axis = vec(1,0,0), length= 5, pos = vec(0,2,20))
 z = arrow(axis = vec(0,1,0), length = 5, pos = vec(0,2,20))
 
 xyplot = graph(title = "x-y plot", xtitle = "x(m)", ytitle = "y(m)", height = 400, width = 600)
-vtplot = graph(title = "v-t plot", xtitle = "t(s)", ytitle = "black:vx(m/s) ; blue:vy(m/s)", height = 400, width = 600)
+vtplot = graph(title = "vx-t plot", xtitle = "t(s)", ytitle = "vx(m/s)", height = 400, width = 600)
+vytplot = graph(title = "vy-t plot", xtitle = "t(s)", ytitle = "vy(m/s)", height = 400, width = 600)
 
 xy = gcurve(graph = xyplot, color = color.red)
 vxt = gcurve(graph = vtplot, color = color.black)
-vyt = gcurve(graph = vtplot, color = color.blue)
+vyt = gcurve(graph = vytplot, color = color.blue)
 
-while ball.pos.y >= (floor.size.y*0.5 + ball.radius):
+while collapse <= 5:
     rate(1000)
     t += dt
     a = vec(0,g,0) - b*v
@@ -33,4 +36,7 @@ while ball.pos.y >= (floor.size.y*0.5 + ball.radius):
 
     xy.plot(ball.pos.x, ball.pos.y)
     vxt.plot(pos = (t,v.x))
-    vyt.plot(pos = (t, abs(v.y)))
+    vyt.plot(pos = (t, v.y))
+    if ball.pos.y <= floor.pos.y*0.5+radius:
+        collapse += 1
+        v.y = -e*v.y
